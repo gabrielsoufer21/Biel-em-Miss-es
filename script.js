b2cee270e6b0047d050f462ae19ad68599e71f72
gabrielsoufer21 <gabrielsoufernandes21@gmail.com>
2026-08-17 21:38:52 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -41,19 +41,7 @@ historiaToggle.addEventListener("click", function () {
 });
 
 
-const europaToggle =
-    document.getElementById("europaToggle");
 
-const europaPanel =
-    document.getElementById("europaPanel");
-
-europaToggle.addEventListener("click", function () {
-
-    europaPanel.classList.toggle("aberto");
-
-    europaToggle.classList.toggle("aberto");
-
-});
 
 /* ================================================= */
 /* DESTAQUE AO PASSAR O MOUSE */
